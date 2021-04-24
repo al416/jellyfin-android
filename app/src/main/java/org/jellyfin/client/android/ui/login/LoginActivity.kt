5cf38ade3155c11371dd2af228432978b6375867
al416 <83140187+al416@users.noreply.github.com>
2021-04-24 19:43:09 -0400

    Added simple login fragment (#2)
    * Added gradle dependencies
    
    * Fix spacing
    
    * Created a simple login fragment

--- app/src/main/java/org/jellyfin/client/android/ui/login/LoginActivity.kt
+++ app/src/main/java/org/jellyfin/client/android/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package org.jellyfin.client.android
+package org.jellyfin.client.android.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jellyfin.client.android.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/org/jellyfin/client/android/ui/login/LoginFragment.kt
@@ -0,0 +1,43 @@
+package org.jellyfin.client.android.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import org.jellyfin.client.android.R
+
+class LoginFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var textBaseUrl: TextInputEditText
+    private lateinit var textUsername: TextInputEditText
+    private lateinit var textPassword: TextInputEditText
+    private lateinit var buttonLogin: Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textBaseUrl = ViewCompat.requireViewById(view, R.id.textBaseUrl)
+        textUsername = ViewCompat.requireViewById(view, R.id.textUsername)
+        textPassword = ViewCompat.requireViewById(view, R.id.textPassword)
+
+        buttonLogin = ViewCompat.requireViewById(view, R.id.buttonLogin)
+        buttonLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.buttonLogin -> {
+
+            }
+        }
+    }
+
+}
