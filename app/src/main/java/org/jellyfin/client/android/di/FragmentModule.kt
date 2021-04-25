894556e479041d4d638c9f4c4d14ad2dbebf274a
al416 <al.amin.1@torlab.nl>
2021-04-24 20:54:10 -0400

    Added home activity and fragment

--- app/src/main/java/org/jellyfin/client/android/di/FragmentModule.kt
+++ app/src/main/java/org/jellyfin/client/android/di/FragmentModule.kt
@@ -2,6 +2,7 @@ package org.jellyfin.client.android.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import org.jellyfin.client.android.ui.home.HomeFragment
 import org.jellyfin.client.android.ui.login.LoginFragment
 
 @Suppress("unused")
@@ -9,4 +10,7 @@ import org.jellyfin.client.android.ui.login.LoginFragment
 abstract class FragmentModule {
     @ContributesAndroidInjector
     abstract fun contributesLoginFragment(): LoginFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributesHomeFragment(): HomeFragment
 }
