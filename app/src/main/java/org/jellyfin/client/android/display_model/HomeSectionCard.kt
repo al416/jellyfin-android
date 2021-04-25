eeb6f37d3cdd12702b40a294f515c24d10dca124
al416 <al.amin.1@torlab.nl>
2021-04-25 00:16:21 -0400

    Implemented home screen recycler view with scrollable rows

---
+++ app/src/main/java/org/jellyfin/client/android/display_model/HomeSectionCard.kt
@@ -0,0 +1,17 @@
+package org.jellyfin.client.android.display_model
+
+data class HomeSectionCard(
+    val id: Int,
+    val backgroundImage: Int,
+    val title: String,
+    val subtitle: String? = null,
+    val rowId: Int,
+    val homeCardType: HomeCardType
+    //val clickListener: View.OnClickListener? = null
+)
+
+enum class HomeCardType {
+    SECTION,    // clicking this card will take the user to Movies/TV Shows list page
+    PLAYABLE_ITEM,  // clicking this card will play the item (i.e. cards under Continue Watching/Next Up)
+    DETAILS // clicking this card will take user to Movie/TV Shows Details page (i.e. under Latest Movies)
+}
---
+++ app/src/main/java/org/jellyfin/client/android/display_model/HomeSectionRow.kt
@@ -0,0 +1,6 @@
+package org.jellyfin.client.android.display_model
+
+data class HomeSectionRow(
+    val id: Int,
+    val title: String
+)
