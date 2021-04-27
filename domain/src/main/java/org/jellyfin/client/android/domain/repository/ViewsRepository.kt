3e6619153bc7dc926203d75e9c4b32050fbf9594
al416 <83140187+al416@users.noreply.github.com>
2021-04-27 18:04:37 -0400

    Implemented data binding for the row recycler view (#14)

--- domain/src/main/java/org/jellyfin/client/android/domain/repository/ViewsRepository.kt
+++ domain/src/main/java/org/jellyfin/client/android/domain/repository/ViewsRepository.kt
@@ -3,20 +3,19 @@ package org.jellyfin.client.android.domain.repository
 import kotlinx.coroutines.flow.Flow
 import org.jellyfin.client.android.domain.models.LibraryDto
 import org.jellyfin.client.android.domain.models.Resource
-import org.jellyfin.client.android.domain.models.display_model.HomeContents
-import org.jellyfin.client.android.domain.models.display_model.HomeSectionCard
+import org.jellyfin.client.android.domain.models.display_model.HomeSectionRow
 import org.jellyfin.client.android.domain.models.display_model.HomeSectionType
 import java.util.*
 
 interface ViewsRepository {
 
-    suspend fun getMyMediaSection(userId: UUID): Flow<Resource<List<HomeSectionCard>>>
+    suspend fun getMyMediaSection(userId: UUID): Flow<Resource<HomeSectionRow>>
 
-    suspend fun getContinueWatchingSection(userId: UUID, mediaTypes: List<String>?): Flow<Resource<List<HomeSectionCard>>>
+    suspend fun getContinueWatchingSection(userId: UUID, mediaTypes: List<String>?): Flow<Resource<HomeSectionRow>>
 
-    suspend fun getNextUpSection(userId: UUID): Flow<Resource<List<HomeSectionCard>>>
+    suspend fun getNextUpSection(userId: UUID): Flow<Resource<HomeSectionRow>>
 
-    suspend fun getLatestSection(userId: UUID, libraries: List<LibraryDto>): Flow<Resource<HomeContents>>
+    suspend fun getLatestSection(userId: UUID, libraries: List<LibraryDto>): Flow<Resource<List<HomeSectionRow>>>
 
     suspend fun getHomeSections(userId: UUID): Flow<Resource<List<HomeSectionType>>>
 }
--- domain/src/main/java/org/jellyfin/client/android/domain/usecase/ObserveNextUpSection.kt
+++ domain/src/main/java/org/jellyfin/client/android/domain/usecase/ObserveNextUpSection.kt
@@ -3,7 +3,7 @@ package org.jellyfin.client.android.domain.usecase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import org.jellyfin.client.android.domain.models.Resource
-import org.jellyfin.client.android.domain.models.display_model.HomeSectionCard
+import org.jellyfin.client.android.domain.models.display_model.HomeSectionRow
 import org.jellyfin.client.android.domain.repository.ViewsRepository
 import java.util.*
 import javax.inject.Inject
@@ -11,9 +11,9 @@ import javax.inject.Named
 
 class ObserveNextUpSection @Inject constructor(@Named("network") dispatcher: CoroutineDispatcher,
                                                private val viewsRepository: ViewsRepository
-) : BaseUseCase<List<HomeSectionCard>, ObserveNextUpSection.RequestParams>(dispatcher) {
+) : BaseUseCase<HomeSectionRow, ObserveNextUpSection.RequestParams>(dispatcher) {
 
-    override suspend fun invokeInternal(params: RequestParams?): Flow<Resource<List<HomeSectionCard>>> {
+    override suspend fun invokeInternal(params: RequestParams?): Flow<Resource<HomeSectionRow>> {
         if (params == null) {
             throw IllegalArgumentException("Expecting valid parameters")
         }
