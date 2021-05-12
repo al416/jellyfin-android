d370eb54c991a858239c0371d46844c89d07f527
al416 <al.amin.1@torlab.nl>
2021-05-11 20:37:52 -0400

    Improved home screen image loading

--- domain/src/main/java/org/jellyfin/client/android/domain/constants/LibraryType.kt
+++ domain/src/main/java/org/jellyfin/client/android/domain/constants/LibraryType.kt
@@ -4,3 +4,9 @@ object LibraryType {
     const val MOVIE = "Movie"
     const val SERIES = "Series"
 }
+
+object ItemType {
+    const val MOVIE = "Movie"
+    const val EPISODE = "Episode"
+    const val TV_CHANNEL = "TvChannel"
+}
